4b1d822fc9b55ec1ee8d897de6536215cba670ce
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-05 11:13:27 +0600

    query api create

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run() {
       const product = await cursor.toArray();
       res.send(product);
     });
+
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
   } finally {
     //   free
   }
